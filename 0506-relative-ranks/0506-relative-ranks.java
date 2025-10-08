b489906fe8bd3b1fcf10860cb49c23f828f38a78
Andrei <kpa6ep@gmail.com>
2025-10-08 23:37:45 +0200

    Time: 9 ms (61.04%), Space: 45.8 MB (66.38%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,44 +1,22 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-        int[] sortScore = Arrays.copyOf(score, score.length);
-        Arrays.sort(sortScore);
-        reverseArrayInPlace(sortScore);
-
-        String[] result = new String[score.length];
+        int RowScoreAndIndex = 2;
+        int[][] pairs = new int[score.length][RowScoreAndIndex];
 
         for (int i = 0; i < score.length; i++) {
-            for (int j = 0; j < sortScore.length; j++) {
-                if (score[i] == sortScore[j]) {
-                    switch (j) {
-                        case 0:
-                            result[i] = "Gold Medal";
-                            break;
-                        case 1:
-                            result[i] = "Silver Medal";
-                            break;
-                        case 2:
-                            result[i] = "Bronze Medal";
-                            break;
-                        default:
-                            result[i] = String.valueOf(j + 1);
-                    }
-                }
-            }
+            pairs[i][0] = i;
+            pairs[i][1] = score[i];
+        }
+
+        Arrays.sort(pairs, (a,b) -> Integer.compare(b[1], a[1]));
+        String[] prizes = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+
+        String[] result = new String[score.length];
+        for (int i = 0; i < score.length; i++) {
+            int maxPrize = 3;
+            result[pairs[i][0]] = i < maxPrize ? prizes[i] : String.valueOf(i + 1);
         }
 
         return result;
-    }
-
-    public void reverseArrayInPlace(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        
-        while (left < right) {
-            int temp = nums[right];
-            nums[right] = nums[left];
-            nums[left] = temp;
-            left++;
-            right--;
-        }
     }
 }
