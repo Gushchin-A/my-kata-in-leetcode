1b6c9a7ebe990f91c2fe9ce6fb233949fbaef2a6
Andrei <kpa6ep@gmail.com>
2025-10-13 22:49:53 +0200

    Time: 3 ms (78.04%), Space: 45.7 MB (58.29%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,28 +1,27 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+        int duplicate = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int digit = Math.abs(nums[i]);
+            int currentIndex = digit - 1;
+            if (nums[currentIndex] > 0) {
+                nums[currentIndex] *= -1;
+            } else {
+                duplicate = digit;
+            }
+        }
+
+        int missing = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                missing = i + 1;
+            }
+        }
+
         int mismatch = 2;
-        int n = nums.length;
-        int sumActual = n * (n + 1) / 2;
-
-        HashSet<Integer> uniqNums = new HashSet<>();
-
-        int sumNums = 0;
-        for (int num : nums) {
-            uniqNums.add(num);
-            sumNums += num;
-        }
-
-        int sumUniq = 0;
-        for (Integer u : uniqNums) {
-            sumUniq += u;
-        }
-
-        int missNumber = sumActual - sumUniq;
-        int duplicate = sumNums - sumUniq;
-
         int[] result = new int[mismatch];
         result[0] = duplicate;
-        result[1] = missNumber;
+        result[1] = missing;
 
         return result;
     }
