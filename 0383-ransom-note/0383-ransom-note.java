53a228821177861dc2c983478275e180ffad2863
Andrei <kpa6ep@gmail.com>
2025-10-15 16:29:58 +0200

    Time: 4 ms (53.08%), Space: 44.7 MB (72.5%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,15 +2,13 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         char[] storage = magazine.toCharArray();
 
-        boolean notCoincidence = true;
         for (int i = 0; i < ransomNote.length(); i++) {
+            boolean notCoincidence = true;
             for (int j = 0 ; j < storage.length; j++) {
                 if (ransomNote.charAt(i) == storage[j]) {
                     storage[j] = '\0';
                     notCoincidence = false;
                     break;
-                } else {
-                    notCoincidence = true;
                 }
             }
             if (notCoincidence) {
