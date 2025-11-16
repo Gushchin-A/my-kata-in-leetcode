1318f6774b70c79e15c5daf5694b8b4d3ae8ff3c
Andrei <kpa6ep@gmail.com>
2025-11-16 20:03:11 +0100

    Time: 0 ms (100%), Space: 43.5 MB (13.35%) - LeetHub

--- 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.java
+++ 2102-find-the-middle-index-in-array/2102-find-the-middle-index-in-array.java
@@ -1,25 +1,22 @@
 class Solution {
     public int findMiddleIndex(int[] nums) {
         int n = nums.length;
-                
-        int[] leftSum = new int[n];
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            leftSum[i] = sum;
-            sum += nums[i];
-        }
-        
+
         int[] rightSum = new int[n];
-        sum = 0;
+        int sum = 0;
         for (int i = n - 1; i >= 0; i--) {
             rightSum[i] = sum;
             sum += nums[i];
         }
-
+                
+        int[] leftSum = new int[n];
+        sum = 0;
         for (int i = 0; i < n; i++) {
+            leftSum[i] = sum;
             if (leftSum[i] == rightSum[i]) {
                 return i;
             }
+            sum += nums[i];
         }
 
         return -1;
