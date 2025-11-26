41d9f7a9f7e5c2b4388d58a63746fb1f117c8eae
Andrei <kpa6ep@gmail.com>
2025-11-26 13:19:12 +0100

    Time: 5 ms (74.51%), Space: 47.2 MB (21.6%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int totalLength = rows * cols;
+
+        int[] flattenGrid = flatten(grid);
+        int[] flattenSwapIndex = new int[totalLength];
+        for (int i = 0; i < totalLength; i++) {
+            int newIndex = (i + k) % totalLength;
+            flattenSwapIndex[newIndex] = flattenGrid[i];
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+        int countCols = 0;
+        for (int i = 0; i < totalLength; i++) {
+            row.add(flattenSwapIndex[i]);
+            countCols++;
+            if (countCols == cols) {
+                result.add(row);
+                row = new ArrayList<>();
+                countCols = 0;
+            }
+        }
+
+        return result;
+    }
+    
+    public static int[] flatten(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int[] flatten = new int[rows * cols];
+
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                flatten[index++] = grid[i][j];
+            }
+        }
+
+        return flatten;
+    }
+}
