5ae57b3c6eddd1734551b24c6142c4fecbfaee18
Andrei <kpa6ep@gmail.com>
2025-12-01 20:44:52 +0100

    Time: 6 ms (10.91%), Space: 48.4 MB (32.65%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -1,24 +1,41 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
         int n = nums.length;
-        int[] result = new int[n];
 
-        int index = 0;
-        for (int num : nums) {
-            if (num % 2 == 0) {
-                result[index] = num;
-                index += 2;
+        int pointer;
+        for (int i = 0; i < n; i++) {
+            boolean indexEven = i % 2 == 0;
+            int current = nums[i];
+
+            if (indexEven) {
+                if (current % 2 != 0) {
+                    pointer = i + 1;
+                    while (pointer < n) {
+                        if (nums[pointer] % 2 == 0) {
+                            nums[i] = nums[pointer];
+                            nums[pointer] = current;
+                            break;
+                        } else {
+                            pointer++;
+                        }
+                    }
+                }
+            } else {
+                if (current % 2 == 0) {
+                    pointer = i + 1;
+                    while (pointer < n) {
+                        if (nums[pointer] % 2 != 0) {
+                            nums[i] = nums[pointer];
+                            nums[pointer] = current;
+                            break;
+                        } else {
+                            pointer++;
+                        }
+                    }
+                }
             }
         }
 
-        index = 1;
-        for (int num : nums) {
-            if (num % 2 != 0) {
-                result[index] = num;
-                index += 2;
-            }
-        }
-
-        return result;
+        return nums;
     }
 }
