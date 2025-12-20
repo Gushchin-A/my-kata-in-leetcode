44f47a2bcbeca20479f78163a47b531f0cfa8139
Andrei <kpa6ep@gmail.com>
2025-12-20 23:54:24 +0100

    Time: 4 ms (98.88%), Space: 47.4 MB (33.93%) - LeetHub

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.java
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.java
@@ -3,32 +3,17 @@ class Solution {
         List<String> result = new ArrayList<>();
 
         for (String w : words) {
-            if (w.indexOf(separator) != -1) {
                 int left = 0;
                 int right = 0;
-
-                while (right < w.length()) {
-                    if (w.charAt(right) == separator) {
-                        if (right == 0) {
-                            left++;
-                            right++;
-                            continue;
+                while (right <= w.length()) {
+                    if (right == w.length() || w.charAt(right) == separator) {
+                        if (left < right) {
+                            result.add(w.substring(left, right));
                         }
-                        if (w.charAt(right - 1) == separator) {
-                            right++;
-                            left++;
-                            continue;
-                        }
-                        result.add(w.substring(left, right));
                         left = right + 1;
-                    } else if (right == w.length() - 1) {
-                        result.add(w.substring(left, right + 1));
                     }
                     right++;
                 }
-            } else {
-                result.add(w);
-            }
         }
 
         return result;
