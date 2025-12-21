a2b3287a5379ee6ec179be74726b952614f6f18b
Andrei <kpa6ep@gmail.com>
2025-12-21 19:26:40 +0100

    Time: 5 ms (97.88%), Space: 44 MB (18.03%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,17 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> mapS = new HashMap<>();
-        Map<Character, Character> mapT = new HashMap<>();
+        int[] isWasS = new int[256];
+        int[] isWasT = new int[256];
 
         for (int i = 0; i < s.length(); i++) {
             char currentS = s.charAt(i);
             char currentT = t.charAt(i);
 
-            if (mapS.containsKey(currentS)) {
-                if (mapS.get(currentS) != currentT) {
-                    return false;
-                }
+            if (isWasS[currentS] != isWasT[currentT]) {
+                return false;
             }
-            if (mapT.containsKey(currentT)) {
-                if (mapT.get(currentT) != currentS) {
-                    return false;
-                }
-            }
-
-            mapS.put(currentS, currentT);
-            mapT.put(currentT, currentS);
+            isWasS[currentS] = i + 1;
+            isWasT[currentT] = i + 1;
         }
 
         return true;
