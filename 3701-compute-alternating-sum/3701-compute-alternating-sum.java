dd799f93cc1eb89e212c48c119d33e59e901e3a3
Andrei <kpa6ep@gmail.com>
2026-01-05 22:50:27 +0100

    Time: 1 ms (97.75%), Space: 46.7 MB (9.52%) - LeetHub

--- 3701-compute-alternating-sum/3701-compute-alternating-sum.java
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -1,18 +1,11 @@
 class Solution {
     public int alternatingSum(int[] nums) {
-        int n = nums.length;
-
         int alternatingSum = 0;
-        int even = 0;
-        int odd = 1;
-        while (even < n && odd < n) {
-            alternatingSum += nums[even] - nums[odd];
-            even += 2;
-            odd += 2;
-        }
-
-        if (n % 2 != 0) {
-            alternatingSum += nums[n - 1];
+        int sign = 1;
+        
+        for (int n : nums) {
+            alternatingSum += n * sign;
+            sign = -sign;
         }
 
         return alternatingSum;
