ce261bac4534e7fcf252913124868f9ec59df3a4
Andrei <kpa6ep@gmail.com>
2026-01-07 16:04:59 +0100

    Time: 22 ms (8.63%), Space: 44.3 MB (77.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -7,22 +7,17 @@ class Solution {
         Map<Character, Integer> letters = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             char charS = s.charAt(i);
+            char charT = t.charAt(i);
             letters.put(charS, letters.getOrDefault(charS, 0) + 1);
+            letters.put(charT, letters.getOrDefault(charT, 0) - 1);
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            char charT = t.charAt(i);
-            letters.put(charT, letters.getOrDefault(charT, 0) - 1);
-
-            if (letters.get(charT) < 0) {
+        for (Integer value : letters.values()) {
+            if (value != 0) {
                 return false;
             }
-
-            if (letters.get(charT) == 0) {
-                letters.remove(charT);
-            }
         }
 
-        return letters.size() == 0;
+        return true;
     }
 }
