35bf01a4d83442ab65b21750376e337d74bfcb32
Andrei <kpa6ep@gmail.com>
2026-01-09 15:51:48 +0100

    Time: 1 ms (44.88%), Space: 63.1 MB (22.03%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+
+        int[] temp = new int[n];
+        k = k % n;
+        for (int i = 0; i < n; i++) {
+            int newIndex = (i + k) % n;
+            temp[newIndex] = nums[i];
+        }
+
+        System.arraycopy(temp, 0, nums, 0, n);
+    }
+}
