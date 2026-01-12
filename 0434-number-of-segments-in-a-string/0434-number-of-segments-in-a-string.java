7012872cc332de165340ae77abc113602a876277
Andrei <kpa6ep@gmail.com>
2026-01-12 16:29:38 +0100

    Time: 1 ms (38.08%), Space: 42.8 MB (19.72%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -1,12 +1,11 @@
 class Solution {
     public int countSegments(String s) {
         String normalized = s.trim();
+        
         if (normalized.isEmpty()) {
             return 0;
         }
 
-        String[] words = normalized.split("\\s+");
-
-        return words.length;
+        return normalized.split("\\s+").length;
     }
 }
