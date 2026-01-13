11f3a406b5a01094a28b0600ad008dbcffae3ea3
Andrei <kpa6ep@gmail.com>
2026-01-13 11:52:02 +0100

    Time: 0 ms (100%), Space: 43 MB (18.48%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -20,48 +20,44 @@ class Solution {
     }
 
     private boolean isFirstRow(String s) {
-        Set<Character> firstRow = Set.of(
-                'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-                'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'
-        );
-
-        for (char c : s.toCharArray()) {
-            if (!firstRow.contains(c)) {
-                return false;
+        String sLower = s.toLowerCase();
+        for (char c : sLower.toCharArray()) {
+            switch (c) {
+                case 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' -> {
+                }
+                default -> {
+                    return false;
+                }
             }
         }
-
         return true;
     }
 
     private boolean isSecondRow(String s) {
-        Set<Character> secondRow = Set.of(
-                'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-                'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'
-        );
-
-        for (char c : s.toCharArray()) {
-            if (!secondRow.contains(c)) {
-                return false;
+        String sLower = s.toLowerCase();
+        for (char c : sLower.toCharArray()) {
+            switch (c) {
+                case 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' -> {
+                }
+                default -> {
+                    return false;
+                }
             }
         }
-
         return true;
     }
 
     private boolean isThirdRow(String s) {
-        Set<Character> thirdRow = Set.of(
-                'z', 'x', 'c', 'v', 'b', 'n', 'm',
-                'Z', 'X', 'C', 'V', 'B', 'N', 'M'
-        );
-
-
-        for (char c : s.toCharArray()) {
-            if (!thirdRow.contains(c)) {
-                return false;
+        String sLower = s.toLowerCase();
+        for (char c : sLower.toCharArray()) {
+            switch (c) {
+                case 'z', 'x', 'c', 'v', 'b', 'n', 'm' -> {
+                }
+                default -> {
+                    return false;
+                }
             }
         }
-
         return true;
     }
 }
