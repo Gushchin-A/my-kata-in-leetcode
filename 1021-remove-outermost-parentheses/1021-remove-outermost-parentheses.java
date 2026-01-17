3938ac7b90734d5284182dc07a87fe7ada7207f8
Andrei <kpa6ep@gmail.com>
2026-01-17 20:36:35 +0100

    Time: 2 ms (99.42%), Space: 43.5 MB (76.13%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,33 +1,21 @@
 class Solution {
     public String removeOuterParentheses(String s) {
         StringBuilder temp = new StringBuilder();
-        
-        int balance = 0;
-        int start = 0;
-        int end = 0;
-        boolean isPrimitive = false;
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            
-            if (current == '(') {
-                balance++;
-                if (balance == 1 && !isPrimitive) {
-                    isPrimitive = true;
-                    start = i + 1;
-                }
-            } else {
-                balance--;
-                if (balance == 0) {
-                    isPrimitive = false;
-                }
-            }
 
-            if (!isPrimitive) {
-                end = i;
-                temp.append(s.substring(start, end));
+        int balance = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (balance > 0) {
+                    temp.append(c);
+                }
+                balance++;
+            } else {
+                if (balance > 1) {
+                    temp.append(c);
+                }
+                balance--;
             }
         }
-
         return new String(temp);
     }
 }
