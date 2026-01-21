ae406aa1ce06499bb96fc686982767c85b8e3872
Andrei <kpa6ep@gmail.com>
2026-01-21 17:30:58 +0100

    Time: 3 ms (4.1%), Space: 43.2 MB (30.6%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
@@ -1,33 +1,27 @@
 class Solution {
     public int rearrangeCharacters(String s, String target) {
         Map<Character, Integer> sCounts = new HashMap<>();
+        Map<Character, Integer> targetCounts = new HashMap<>();
 
         for (char c : s.toCharArray()) {
             sCounts.merge(c, 1, Integer::sum);
         }
 
-        int result = 0;
+        for (char c : target.toCharArray()) {
+            targetCounts.merge(c, 1, Integer::sum);
+        }
 
-        boolean isGood = true;
-        while (isGood) {
-            for (char c : target.toCharArray()) {
-                if (!sCounts.containsKey(c)) {
-                    isGood = false;
-                    break;
-                } else {
-                    if (sCounts.get(c) <= 0) {
-                        isGood = false;
-                        break;
-                    } else {
-                        sCounts.merge(c, -1, Integer::sum);
-                    }
-                }
-            }
-            if (isGood) {
-                result += 1;
+        int maxTargetChars = 100;
+        int min = maxTargetChars;
+        for (Character key : targetCounts.keySet()) {
+            if (sCounts.containsKey(key)) {
+                int candidate = sCounts.getOrDefault(key, 0) / targetCounts.get(key);
+                min = Math.min(min, candidate);
+            } else {
+                return 0;
             }
         }
 
-        return result;
+        return min;
     }
 }
