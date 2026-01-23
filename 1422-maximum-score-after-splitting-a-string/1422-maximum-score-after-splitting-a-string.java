734c741dbeb0e5dd71fc9c52135a5895b7f38cd3
Andrei <kpa6ep@gmail.com>
2026-01-23 13:23:37 +0100

    Time: 1 ms (99.28%), Space: 42.8 MB (57.57%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -2,43 +2,25 @@ class Solution {
     public int maxScore(String s) {
         int n = s.length();
 
-        int i = 0;
-        int max = 0;
-        while (i < n - 1) {
-            int maxLeft = sumInArrayNumZero(s.substring(0, i + 1).toCharArray());
-            int maxRight = sumInArrayNumOne(s.substring(i + 1, n).toCharArray());
+        int sumRight = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                sumRight++;
+            }
+        }
 
-            max = Math.max(max, maxLeft + maxRight);
-            
-            if (max == n) {
-                return max;
+        int max = 0;
+        int sumLeft = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == '0') {
+                sumLeft++;
+            } else {
+                sumRight--;
             }
 
-            i++;
+            max = Math.max(max, sumLeft + sumRight);
         }
 
         return max;
-    }
-
-    public int sumInArrayNumZero(char[] charNums) {
-        int sum = 0;
-        for (char c : charNums) {
-            if (c == '0') {
-                sum++;
-            }
-        }
-
-        return sum;
-    }
-
-    public int sumInArrayNumOne(char[] charNums) {
-        int sum = 0;
-        for (char c : charNums) {
-            if (c == '1') {
-                sum++;
-            }
-        }
-
-        return sum;
     }
 }
