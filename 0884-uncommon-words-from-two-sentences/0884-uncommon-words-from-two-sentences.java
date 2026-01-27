e76201cd54fb3c19f6b3c5bae67951728d1b00b3
Andrei <kpa6ep@gmail.com>
2026-01-27 19:24:48 +0100

    Time: 6 ms (11.2%), Space: 43.9 MB (9.31%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.java
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.java
@@ -1,20 +1,22 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        String sAll = String.join(" ", s1, s2);
         HashMap<String, Integer> sCounts = new HashMap<>();
 
-        for (String word : sAll.split(" ")) {
+        for (String word : s1.split("\\s+")) {
             sCounts.merge(word, 1, Integer::sum);
         }
 
-        String[] result = new String[s1.length() + s2.length()];
-        int indexRes = 0;
+        for (String word : s2.split("\\s+")) {
+            sCounts.merge(word, 1, Integer::sum);
+        }
+
+        ArrayList<String> result = new ArrayList<>(sCounts.size());
         for (Map.Entry<String, Integer> word : sCounts.entrySet()) {
             if (word.getValue() == 1) {
-                result[indexRes++] = word.getKey();
+                result.add(word.getKey());
             }
         }
-        
-        return Arrays.copyOfRange(result, 0, indexRes);
+
+        return result.toArray(new String[0]);
     }
 }
