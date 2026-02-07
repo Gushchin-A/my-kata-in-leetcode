7ae989c42a8b90849ee02db68f8e50aed60a8948
Andrei <kpa6ep@gmail.com>
2026-02-07 12:54:42 +0100

    Time: 6 ms (26.3%), Space: 46.6 MB (78.12%) - LeetHub

---
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> intersection(int[][] nums) {
+        Map<Integer, Integer> counts = new TreeMap<>();
+
+        for (int[] row : nums) {
+            for (int num : row) {
+                counts.merge(num, 1, Integer::sum);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int needCount = nums.length;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() == needCount) {
+                result.add(entry.getKey());
+            }
+        }
+
+        return result;
+    }
+}
