0839f2ad66644427d8b705dff11036e99f9da6c5
Andrei <kpa6ep@gmail.com>
2026-02-12 20:09:47 +0100

    Time: 4 ms (25.32%), Space: 44.2 MB (24.43%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
@@ -3,22 +3,24 @@ class Solution {
 
     public int maxFreqSum(String s) {
         Map<Character, Integer> freq = new HashMap<>();
-
+        
         for (char c : s.toCharArray()) {
             freq.merge(c, 1, Integer::sum);
         }
 
-        int maxVowel = freq.entrySet().stream()
-                .filter(e -> vowels.contains(e.getKey()))
-                .map(Map.Entry::getValue)
-                .max(Integer::compareTo)
-                .orElse(0);
-
-        int maxConsonant = freq.entrySet().stream()
-                .filter(e -> !vowels.contains(e.getKey()))
-                .map(Map.Entry::getValue)
-                .max(Integer::compareTo)
-                .orElse(0);
+        int maxVowel = 0;
+        int maxConsonant = 0;
+        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+            
+            char currentChar = entry.getKey();
+            int currentCount = entry.getValue();
+            
+            if (vowels.contains(currentChar)) {
+                maxVowel = Math.max(currentCount, maxVowel);
+            } else {
+                maxConsonant = Math.max(currentCount, maxConsonant);
+            }
+        }
 
         return maxVowel + maxConsonant;
     }
