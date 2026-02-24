5a927219e4b4a186ebba0cd92c59a701ded6e07b
Andrei <kpa6ep@gmail.com>
2026-02-24 16:44:41 +0100

    Time: 16 ms (16.94%), Space: 43.4 MB (10.12%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.java
+++ 1399-count-largest-group/1399-count-largest-group.java
@@ -4,22 +4,30 @@ class Solution {
 
         int maxSizeGroup = 0;
         for (int i = 1; i <= n; i++) {
-            int num = i;
-            int sum = 0;
-            while (num > 0) {
-                int digit = num % 10;
-                sum += digit;
-                num /= 10;
-            }
+            int sum = sumDigitsInNumber(i);
             sumAndGroups.merge(sum, 1, Integer::sum);
             maxSizeGroup = Math.max(maxSizeGroup, sumAndGroups.get(sum));
         }
 
-        int finalMaxSizeGroup = maxSizeGroup;
+        int result = 0;
+        for (int size : sumAndGroups.values()) {
+            if (size == maxSizeGroup) {
+                result++;
+            }
+        }
 
-        return (int) sumAndGroups.values()
-                .stream()
-                .filter(value -> value == finalMaxSizeGroup)
-                .count();
+        return result;
+    }
+
+    private int sumDigitsInNumber(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit;
+            num /= 10;
+        }
+
+        return sum;
     }
 }
