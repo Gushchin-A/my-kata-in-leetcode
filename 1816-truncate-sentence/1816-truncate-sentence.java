d7254e2f5c515251b0bdcb67c22da1ef83d375fa
Andrei <kpa6ep@gmail.com>
2026-03-02 22:59:59 +0100

    Time: 4 ms (16.08%), Space: 43.2 MB (70.98%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        StringBuilder result = new StringBuilder();
+
+        int seen = 0;
+        for (String word : s.split("\\s+")) {
+            if (seen == k) {
+                break;
+            }
+            result.append(word);
+            result.append(" ");
+            seen++;
+        }
+        result.setLength(result.length() - 1);
+
+        return result.toString();
+    }
+}
