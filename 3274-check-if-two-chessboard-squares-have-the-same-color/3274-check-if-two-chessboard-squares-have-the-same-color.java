f932b9870b0213da80e4a797702e0488598b0f89
Andrei <kpa6ep@gmail.com>
2026-03-31 00:37:38 +0800

    Time: 0 ms (100%), Space: 43.3 MB (65.93%) - LeetHub

--- 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
@@ -1,29 +1,15 @@
 class Solution {
     private static final int[] lettersLine = {1, 2, 3, 4, 5, 6, 7, 8};
-    private static final String black = "black";
-    private static final String white = "white";
 
     public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
-        String color1;
         int first1 = lettersLine[coordinate1.charAt(0) - 'a'];
         int second1 = coordinate1.charAt(1) - '0';
-        if (first1 % 2 == 0 && second1 % 2 == 0
-                || first1 % 2 != 0 && second1 % 2 != 0) {
-            color1 = black;
-        } else {
-            color1 = white;
-        }
+        boolean color1 = first1 % 2 == second1 % 2;
         
-        String color2;
         int first2 = lettersLine[coordinate2.charAt(0) - 'a'];
         int second2 = coordinate2.charAt(1) - '0';
-        if (first2 % 2 == 0 && second2 % 2 == 0
-                || first2 % 2 != 0 && second2 % 2 != 0) {
-            color2 = black;
-        } else {
-            color2 = white;
-        }
+        boolean color2 = first2 % 2 == second2 % 2;
         
-        return color1.equals(color2);
+        return color1 == color2;
     }
 }
