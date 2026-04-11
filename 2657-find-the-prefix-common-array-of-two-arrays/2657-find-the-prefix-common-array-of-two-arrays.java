3068dcf3b24f75cbc7b9f0b9f871dbe89466aee8
Andrei <kpa6ep@gmail.com>
2026-04-12 04:20:12 +0800

    Time: 3 ms (53.97%), Space: 47.3 MB (79.04%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
@@ -5,17 +5,20 @@ class Solution {
         for (int i = 0; i < C.length; i++) {
 
             boolean[] seen = new boolean[51];
-            for (int j = 0; j <= i; j++) {
-                seen[A[j]] = true;
-            }
-
             int count = 0;
             for (int j = 0; j <= i; j++) {
+                if (seen[A[j]]) {
+                    count++;
+                } else {
+                    seen[A[j]] = true;
+                }
+
                 if (seen[B[j]]) {
                     count++;
+                } else {
+                    seen[B[j]] = true;
                 }
             }
-
             C[i] = count;
         }
 
