721846a5c47f7fa3675fd48ee468770573c4ce72
Andrei <kpa6ep@gmail.com>
2026-05-07 02:33:27 +0800

    Time: 0 ms (100%), Space: 41.9 MB (80.68%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,21 +1,22 @@
 class Solution {
-    private static final int[] firstWeek = {1, 2, 3, 4, 5, 6, 7};
-    private static final int sumFirstWeek = 28;
-    private static final int amountExtraWeek = 7;
+    private static final int[] FIRST_WEEK = {1, 2, 3, 4, 5, 6, 7};
+    private static final int SUM_FIRST_WEEK = 28;
+    private static final int AMOUNT_EXTRA_WEEK = 7;
+    private static final int DAYS_IN_WEEK = 7;
 
     public int totalMoney(int n) {
         int totalAmount = 0;
-        int fullWeeks = n / 7;
+        int fullWeeks = n / DAYS_IN_WEEK;
 
         if (fullWeeks > 0) {
             for (int i = 0; i < fullWeeks; i++) {
-                totalAmount += sumFirstWeek + i * amountExtraWeek;
+                totalAmount += SUM_FIRST_WEEK + i * AMOUNT_EXTRA_WEEK;
             }
         }
 
-        int days = n % 7;
+        int days = n % DAYS_IN_WEEK;
         for (int i = 0; i < days; i++) {
-            totalAmount += firstWeek[i] + fullWeeks;
+            totalAmount += FIRST_WEEK[i] + fullWeeks;
         }
 
         return totalAmount;      
