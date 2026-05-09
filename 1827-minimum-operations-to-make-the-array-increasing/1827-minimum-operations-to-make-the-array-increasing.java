71ffd686b8827f015d3b1baf7d2da59e0af0c1b5
Andrei <kpa6ep@gmail.com>
2026-05-09 17:25:47 +0800

    Time: 2 ms (100%), Space: 46.7 MB (94.06%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int[] copyNums = Arrays.copyOf(nums, nums.length);
-
         int operations = 0;
-        int tempMax = 0;
-        for (int i = 0; i < copyNums.length - 1; i++) {
-            if (copyNums[i] >= copyNums[i + 1]) {
-                tempMax = copyNums[i] + 1;
-                operations += tempMax - copyNums[i + 1];
-                copyNums[i + 1] = tempMax;
+        int tempMax = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= tempMax) {
+                tempMax++;
+                operations += tempMax - nums[i];
+            } else {
+                tempMax = nums[i];
             }
         }
 
