bb486b1a69c4d64e032cdda84752092d68c64cf9
Andrei <kpa6ep@gmail.com>
2026-05-14 01:51:53 +0800

    Time: 0 ms (100%), Space: 43.2 MB (27.48%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
@@ -15,23 +15,6 @@
  */
 class Solution {
     public boolean checkTree(TreeNode root) {
-        int[] sumChildren = new int[1];
-        helper(root, sumChildren);
-
-        return root.val == sumChildren[0];
-    }
-
-    private void helper(TreeNode node, int[] sumChildren) {
-        if (node == null) {
-            return;
-        }
-
-        if (node.left == null && node.right == null) {
-            return;
-        }
-
-        sumChildren[0] += node.left.val + node.right.val;
-        helper(node.left, sumChildren);
-        helper(node.right, sumChildren);
+        return root.val == root.left.val + root.right.val;
     }
 }
