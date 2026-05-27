20d062148e5005541b16cff2e665505bf1f9a6c6
Andrei <kpa6ep@gmail.com>
2026-05-27 22:35:49 +0800

    Time: 1 ms (64.89%), Space: 43 MB (90.19%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,36 +1,12 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        Set<String> uniqBinaryNums = new HashSet<>();
-        Collections.addAll(uniqBinaryNums, nums);
+        StringBuilder uniqString = new StringBuilder();
 
-        String result = "";
-        for (String binaryString : nums) {
-
-            StringBuilder candidate = new StringBuilder();
-            for (char c : binaryString.toCharArray()) {
-                candidate.append(c);
-            }
-
-            for (int i = 0; i < candidate.length(); i++) {
-                if (candidate.charAt(i) == '1') {
-                    candidate.setCharAt(i, '0');
-                } else {
-                    candidate.setCharAt(i, '1');
-                }
-
-                if (!uniqBinaryNums.contains(candidate.toString())) {
-                    result = candidate.toString();
-                    break;
-                } else {
-                    if (candidate.charAt(i) == '1') {
-                        candidate.setCharAt(i, '0');
-                    } else {
-                        candidate.setCharAt(i, '1');
-                    }
-                }
-            }
+        for (int i = 0; i < nums.length; i++) {
+            char c = nums[i].charAt(i);
+            uniqString.append(c == '1' ? '0' : '1');
         }
 
-        return result;
+        return uniqString.toString();
     }
 }
