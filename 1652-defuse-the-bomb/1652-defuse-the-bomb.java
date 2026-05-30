d2a59b780d619d04212bb34f5b6f45ec520898ae
Andrei <kpa6ep@gmail.com>
2026-05-30 21:33:23 +0800

    Time: 0 ms (100%), Space: 44 MB (12.76%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -2,19 +2,27 @@ class Solution {
     public int[] decrypt(int[] code, int k) {
         int n = code.length;
         int[] temp = new int[n];
+        int sum = 0;
 
         if (k > 0) {
-            for (int i = 0; i < n; i++) {
-                int sum = 0;
 
-                int j = i + 1;
-                int index = 0;
-                while (index < k) {
-                    sum += code[j % n];
-                    index++;
-                    j++;
-                }
-                temp[i] = sum;
+            for (int i = 1; i <= k; i++) {
+                sum += code[i % n];
+            }
+
+            temp[0] = sum;
+
+            int left = 1;
+            int right = k;
+            int indexResult = 1;
+            while (indexResult < n) {
+                sum -= code[left % n];
+                left++;
+
+                right++;
+                sum += code[right % n];
+
+                temp[indexResult++] = sum;
             }
 
             for (int i = 0; i < n; i++) {
@@ -23,17 +31,26 @@ class Solution {
         }
 
         if (k < 0) {
-            for (int i = 0; i < n; i++) {
-                int sum = 0;
 
-                int j = i - 1 + n;
-                int index = 0;
-                while (index > k) {
-                    sum += code[j % n];
-                    index--;
-                    j--;
-                }
-                temp[i] = sum;
+            k = -k;
+
+            for (int i = -1 + n; i >= n - k; i--) {
+                sum += code[i % n];
+            }
+
+            temp[0] = sum;
+
+            int left = n - k;
+            int right = n - 1;
+            int indexResult = 1;
+            while (indexResult < n) {
+                sum -= code[left % n];
+                left++;
+
+                right++;
+                sum += code[right % n];
+
+                temp[indexResult++] = sum;
             }
 
             for (int i = 0; i < n; i++) {
