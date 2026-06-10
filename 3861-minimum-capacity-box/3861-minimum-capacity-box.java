0972eb30bc6a1b15dbe54d9afcc5d9649725ad5f
Andrei <kpa6ep@gmail.com>
2026-06-10 23:02:41 +0800

    Time: 0 ms (100%), Space: 44.8 MB (25.7%) - LeetHub

--- 3861-minimum-capacity-box/3861-minimum-capacity-box.java
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.java
@@ -1,20 +1,17 @@
 class Solution {
     public int minimumIndex(int[] capacity, int itemSize) {
-        int noValidBox = -1;
-        boolean hasValidBox = false;
-
-        int smallestIndex = 0; 
+        int result = -1; 
         int minBoxSize = Integer.MAX_VALUE;
+
         for (int i = 0; i < capacity.length; i++) {
             if (capacity[i] >= itemSize) {
-                hasValidBox = true;
                 if (capacity[i] < minBoxSize) {
-                    smallestIndex = i;
+                    result = i;
                     minBoxSize = capacity[i];
                 }
             }
         }
 
-        return hasValidBox == true ? smallestIndex : noValidBox;
+        return result;
     }
 }
