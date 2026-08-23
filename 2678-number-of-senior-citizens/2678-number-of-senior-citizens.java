1eb584d29568ad9434745140584024d1d02b76a8
Andrei <kpa6ep@gmail.com>
2026-08-23 17:47:35 +0200

    Time: 1 ms (65.68%), Space: 44.3 MB (44.47%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -3,14 +3,8 @@ class Solution {
         int count = 0;
 
         for (String info : details) {
-            char firstDigtit = info.charAt(info.length() - 4);
-            char secondDigtit = info.charAt(info.length() - 3);
-            
-            if (firstDigtit == '6' && secondDigtit == '0') {
-                continue;
-            }
-
-            if (firstDigtit >= '6') {
+            int age = Integer.parseInt(info.substring(11, 13));
+            if (age > 60) {
                 count++;
             }
         }
