adbe6921d6b9eb8f02d8b4aa7277fed42ab30778
Andrei <kpa6ep@gmail.com>
2026-09-02 21:51:39 +0200

    Time: 3 ms (46.08%), Space: 46.7 MB (83.68%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
@@ -4,9 +4,9 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             int count = 0;
-            boolean isEven = nums[i] % 2 == 0 ? true : false; 
+            boolean isEven = nums[i] % 2 == 0; 
             for (int j = i + 1; j < nums.length; j++) {
-                boolean isOdd = nums[j] % 2 != 0 ? true : false;
+                boolean isOdd = nums[j] % 2 != 0;
                 if (isEven == isOdd) {
                     count++;
                 }
