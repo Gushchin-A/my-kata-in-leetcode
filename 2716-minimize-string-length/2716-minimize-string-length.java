6855cdae9e5ff740457e4833d3ec574a285e61f4
Andrei <kpa6ep@gmail.com>
2026-09-04 11:32:13 +0200

    Time: 7 ms (79.61%), Space: 47.1 MB (25.96%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.java
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -1,17 +1,10 @@
 class Solution {
     public int minimizedStringLength(String s) {
-        int[] freq = new int[26];
+        Set<Character> uniq = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
+            uniq.add(s.charAt(i));
         }
 
-        int result = 0;
-        for (int value : freq) {
-            if (value > 0) {
-                result++;
-            }
-        }
-
-        return result;
+        return uniq.size();
     }
 }
